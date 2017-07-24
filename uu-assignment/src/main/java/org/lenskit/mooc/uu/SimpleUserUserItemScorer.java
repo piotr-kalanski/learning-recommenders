e29e10ddaf299e07affd545461761716a991d51d
kalan <piotr.kalanski@gmail.com>
2017-07-24 21:59:52 +0200

    User-user recommender - template

---
+++ uu-assignment/src/main/java/org/lenskit/mooc/uu/SimpleUserUserItemScorer.java
@@ -0,0 +1,71 @@
+package org.lenskit.mooc.uu;
+
+import com.google.common.collect.Maps;
+import it.unimi.dsi.fastutil.longs.Long2DoubleMap;
+import it.unimi.dsi.fastutil.longs.Long2DoubleOpenHashMap;
+import it.unimi.dsi.fastutil.longs.Long2DoubleSortedMap;
+import org.lenskit.api.Result;
+import org.lenskit.api.ResultMap;
+import org.lenskit.basic.AbstractItemScorer;
+import org.lenskit.data.dao.DataAccessObject;
+import org.lenskit.data.entities.CommonAttributes;
+import org.lenskit.data.ratings.Rating;
+import org.lenskit.results.Results;
+import org.lenskit.util.ScoredIdAccumulator;
+import org.lenskit.util.TopNScoredIdAccumulator;
+import org.lenskit.util.collections.LongUtils;
+import org.lenskit.util.math.Scalars;
+import org.lenskit.util.math.Vectors;
+
+import javax.annotation.Nonnull;
+import javax.inject.Inject;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * User-user item scorer.
+ * @author <a href="http://www.grouplens.org">GroupLens Research</a>
+ */
+public class SimpleUserUserItemScorer extends AbstractItemScorer {
+    private final DataAccessObject dao;
+    private final int neighborhoodSize;
+
+    /**
+     * Instantiate a new user-user item scorer.
+     * @param dao The data access object.
+     */
+    @Inject
+    public SimpleUserUserItemScorer(DataAccessObject dao) {
+        this.dao = dao;
+        neighborhoodSize = 30;
+    }
+
+    @Nonnull
+    @Override
+    public ResultMap scoreWithDetails(long user, @Nonnull Collection<Long> items) {
+        // TODO Score the items for the user with user-user CF
+        throw new UnsupportedOperationException("not yet implemented");
+    }
+
+    /**
+     * Get a user's rating vector.
+     * @param user The user ID.
+     * @return The rating vector, mapping item IDs to the user's rating
+     *         for that item.
+     */
+    private Long2DoubleOpenHashMap getUserRatingVector(long user) {
+        List<Rating> history = dao.query(Rating.class)
+                                  .withAttribute(CommonAttributes.USER_ID, user)
+                                  .get();
+
+        Long2DoubleOpenHashMap ratings = new Long2DoubleOpenHashMap();
+        for (Rating r: history) {
+            ratings.put(r.getItemId(), r.getValue());
+        }
+
+        return ratings;
+    }
+
+}
