9aecfab7f8366803fe412835e3942cf7eed0ebd1
kalan <piotr.kalanski@gmail.com>
2017-07-29 15:15:03 +0200

    item item recommender - template

---
+++ ii-assignment/src/main/java/org/lenskit/mooc/ii/SimpleItemBasedItemScorer.java
@@ -0,0 +1,42 @@
+package org.lenskit.mooc.ii;
+
+import org.lenskit.api.Result;
+import org.lenskit.api.ResultMap;
+import org.lenskit.basic.AbstractItemBasedItemScorer;
+import org.lenskit.results.Results;
+
+import javax.annotation.Nonnull;
+import javax.inject.Inject;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Global item scorer to find similar items.
+ * @author <a href="http://www.grouplens.org">GroupLens Research</a>
+ */
+
+public class SimpleItemBasedItemScorer extends AbstractItemBasedItemScorer {
+    private final SimpleItemItemModel model;
+
+    @Inject
+    public SimpleItemBasedItemScorer(SimpleItemItemModel mod) {
+        model = mod;
+    }
+
+    /**
+     * Score items with respect to a set of reference items.
+     * @param basket The reference items.
+     * @param items The score vector. Its domain is the items to be scored, and the scores should
+     *               be stored into this vector.
+     */
+    @Override
+    public ResultMap scoreRelatedItemsWithDetails(@Nonnull Collection<Long> basket, Collection<Long> items) {
+        List<Result> results = new ArrayList<>();
+
+        // TODO Score the items and put them in results
+
+        return Results.newResultMap(results);
+    }
+}
---
+++ ii-assignment/src/main/java/org/lenskit/mooc/ii/SimpleItemItemScorer.java
@@ -0,0 +1,79 @@
+package org.lenskit.mooc.ii;
+
+import it.unimi.dsi.fastutil.longs.Long2DoubleMap;
+import it.unimi.dsi.fastutil.longs.Long2DoubleOpenHashMap;
+import org.lenskit.api.Result;
+import org.lenskit.api.ResultMap;
+import org.lenskit.basic.AbstractItemScorer;
+import org.lenskit.data.dao.DataAccessObject;
+import org.lenskit.data.entities.CommonAttributes;
+import org.lenskit.data.ratings.Rating;
+import org.lenskit.results.Results;
+import org.lenskit.util.ScoredIdAccumulator;
+import org.lenskit.util.TopNScoredIdAccumulator;
+
+import javax.annotation.Nonnull;
+import javax.inject.Inject;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author <a href="http://www.grouplens.org">GroupLens Research</a>
+ */
+public class SimpleItemItemScorer extends AbstractItemScorer {
+    private final SimpleItemItemModel model;
+    private final DataAccessObject dao;
+    private final int neighborhoodSize;
+
+    @Inject
+    public SimpleItemItemScorer(SimpleItemItemModel m, DataAccessObject dao) {
+        model = m;
+        this.dao = dao;
+        neighborhoodSize = 20;
+    }
+
+    /**
+     * Score items for a user.
+     * @param user The user ID.
+     * @param items The score vector.  Its key domain is the items to score, and the scores
+     *               (rating predictions) should be written back to this vector.
+     */
+    @Override
+    public ResultMap scoreWithDetails(long user, @Nonnull Collection<Long> items) {
+        Long2DoubleMap itemMeans = model.getItemMeans();
+        Long2DoubleMap ratings = getUserRatingVector(user);
+
+        // TODO Normalize the user's ratings by subtracting the item mean from each one.
+
+        List<Result> results = new ArrayList<>();
+
+        for (long item: items ) {
+            // TODO Compute the user's score for each item, add it to results
+        }
+
+        return Results.newResultMap(results);
+
+    }
+
+    /**
+     * Get a user's ratings.
+     * @param user The user ID.
+     * @return The ratings to retrieve.
+     */
+    private Long2DoubleOpenHashMap getUserRatingVector(long user) {
+        List<Rating> history = dao.query(Rating.class)
+                                  .withAttribute(CommonAttributes.USER_ID, user)
+                                  .get();
+
+        Long2DoubleOpenHashMap ratings = new Long2DoubleOpenHashMap();
+        for (Rating r: history) {
+            ratings.put(r.getItemId(), r.getValue());
+        }
+
+        return ratings;
+    }
+
+
+}
